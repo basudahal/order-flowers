f8ab260c9cadb9af4caae2d156191a94c9681c3e
System Administrator <root@basudevs-air.lan>
2020-12-01 09:04:11 -0700

    pipeline update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,10 +20,13 @@ pipeline {
                     //buildName "${params.dev_tag}-${params.environment}"
                     //BUILD_DISPLAY_NAME "${params.dev_tag}-${params.environment}-${params.region}"
                     sh "git checkout ${params.dev_tag}"
+                    sh "zip -r OrderFlowers_Lambda.zip index.js"
                     sh "zip -r OrderFlowers_Export.zip OrderFlowers_Export.json"
+                    //sh "aws iam create-role --role-name lambda-ex --assume-role-policy-document '{"Version": "2012-10-17","Statement": [{ "Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"}, "Action": "sts:AssumeRole"}]}'"
+                    //sh "aws iam attach-role-policy --role-name lambda-ex --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
                     sh "ls -la"
                     script {
-                      deployLambda = "serverless deploy"
+                      deployLambda = "aws lambda create-function --function-name order-flowers-lambda --zip-file fileb://./OrderFlowers_Lambda.zip --handler index.handler --runtime nodejs12.x --role arn:aws:iam::878955458484:role/lambda-ex"
                       deployLex = "aws lex-models start-import --payload fileb://./OrderFlowers_Export.zip --resource-type BOT --merge-strategy OVERWRITE_LATEST --region us-east-1"
                       deployAccount = deploymentAccount
                     }
